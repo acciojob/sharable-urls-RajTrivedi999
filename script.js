3a8e41d246b699907fef68aa20888b91170312f4
raj trivedi <113424414+RajTrivedi999@users.noreply.github.com>
2023-01-27 14:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // your code here
+const form=document.getElementById('myDoc');
+const name = form.elements['name'];
+const email = form.elements['year'];
+let fullName = name.value;
+let myYear = year.value;
